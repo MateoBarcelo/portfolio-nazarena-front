519318caf095c8dfb74e52dda68d55b5ede9f2c0
MateoBarcelo <mateobarcelo88@gmail.com>
2025-04-17 15:52:06 -0300

    feat: add projects data file and update project service to use local JSON

--- src/services/project.js
+++ src/services/project.js
@@ -1,36 +1,32 @@
-import axios from 'axios'
+import axios from "axios";
+import projectsJson from "../assets/projects.json";
 
-const baseUrl = `${import.meta.env.VITE_APIURL}/api/projects`
-const mediaUrl = `${import.meta.env.VITE_APIURL}/api/upload`
+const baseUrl = `${import.meta.env.VITE_APIURL}/api/projects`;
+const mediaUrl = `${import.meta.env.VITE_APIURL}/api/upload`;
 
 const create = async (newProject, token) => {
-    
-    const config = {
-        headers: {
-            Authorization: `Bearer ${token}`
-        }
-    }
+  const config = {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  };
 
-    const response = await axios.post(baseUrl, newProject, config)
-    return response.data
-}
+  const response = await axios.post(baseUrl, newProject, config);
+  return response.data;
+};
 
 const uploadImage = async (formData, token) => {
-    const config = {
-        headers: {
-            Authorization: `Bearer ${token}`
-        }
-    }
-    const response = await axios.post(mediaUrl, formData, config)
-    return response.data
-}
+  const config = {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  };
+  const response = await axios.post(mediaUrl, formData, config);
+  return response.data;
+};
 
 const getAll = async () => {
-    const response = await fetch("../src/assets/projects.json")
+  return projectsJson.projects;
+};
 
-    const data = await response.json()
-
-    return data.projects
-}
-
-export default {create, getAll, uploadImage}
+export default { create, getAll, uploadImage };
