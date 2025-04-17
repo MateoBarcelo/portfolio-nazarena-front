446b4b3255fe948b544d44e45a4d90d68472a471
MateoBarcelo <mateobarcelo88@gmail.com>
2025-04-17 17:59:28 -0300

    feat: add Services component with service packs and integrate ServiceGrid

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,18 +1,22 @@
-import PropTypes from 'prop-types'
+import PropTypes from "prop-types";
 
-export default function Button({children, className, type, onClick}) {
-    return(
-        <button type={type} className={`bg-secondary-300 py-1 px-3 font-semibold text-primary-900 text-lg
+export default function Button({ children, className, type, onClick }) {
+  return (
+    <button
+      type={type}
+      className={`bg-secondary-300 py-1 px-3 font-semibold text-primary-900 text-lg
          rounded-lg hover:bg-secondary-100 hover:shadow-md transition-all active:transform 
-         active:translate-y-1 z-20 ${className}` } onClick={onClick}>
-            {children}
-        </button>
-    )
+         active:translate-y-1 z-20 ${className}`}
+      onClick={onClick}
+    >
+      {children}
+    </button>
+  );
 }
 
 Button.propTypes = {
-    children: PropTypes.string,
-    className: PropTypes.string,
-    type: PropTypes.string,
-    onClick: PropTypes.func
-}
+  children: PropTypes.string,
+  className: PropTypes.string,
+  type: PropTypes.string,
+  onClick: PropTypes.func,
+};
---
+++ src/components/pages/Services.jsx
@@ -0,0 +1,27 @@
+import { motion } from "framer-motion";
+import useServices from "../../hooks/useServices.jsx";
+import ServiceGrid from "../services/ServiceGrid.jsx";
+
+export default function Services() {
+  document.documentElement.scrollTop = 0;
+
+  const { services } = useServices();
+
+  return (
+    <motion.div
+      initial={{ width: 0 }}
+      animate={{ width: "100%" }}
+      exit={{ x: window.innerWidth, transition: { duration: 0.25 } }}
+    >
+      <div className="text-center text-white flex flex-col justify-center items-center">
+        <p className="text-4xl font-semibold mt-16">Packs de diseño</p>
+        <p className="text-lg md:text-xl py-6 opacity-75 max-w-4xl">
+          Soluciones de diseño completas para emprendedores y pequeñas empresas.
+          Cada pack está cuidadosamente creado para ayudarte a destacar en el
+          mercado.
+        </p>
+      </div>
+      <ServiceGrid services={services}></ServiceGrid>
+    </motion.div>
+  );
+}
