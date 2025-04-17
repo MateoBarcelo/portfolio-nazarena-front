707918a739aabed85dd0a7041e79da150d51909c
MateoBarcelo <mateobarcelo88@gmail.com>
2025-04-17 17:59:11 -0300

    feat: add Services component to routes and update Header links

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,15 +1,14 @@
-import HeaderItem from './HeaderItem.jsx'
+import HeaderItem from "./HeaderItem.jsx";
 
 export default function Header() {
-
-    return (
-            <header className="sticky top-0 z-50 max-w-full shadow-lg h-10 md:h-12 bg-white flex justify-center align-center text-xs md:text-base">
-                <ul className="flex justify-center items-center px-4 space-x-4 font-semibold">
-                    <HeaderItem to="/">INICIO</HeaderItem>
-                    <HeaderItem to="/proyectos">PROYECTOS</HeaderItem>
-                    <HeaderItem to="/contacto">CONTACTO</HeaderItem>
-                </ul>
-            </header>
-
-    )
+  return (
+    <header className="sticky top-0 z-50 max-w-full shadow-lg h-10 md:h-12 bg-white flex justify-center align-center text-xs md:text-base">
+      <ul className="flex justify-center items-center px-4 space-x-4 font-semibold">
+        <HeaderItem to="/">INICIO</HeaderItem>
+        <HeaderItem to="/proyectos">PROYECTOS</HeaderItem>
+        <HeaderItem to="/tienda">TIENDA</HeaderItem>
+        <HeaderItem to="/contacto">CONTACTO</HeaderItem>
+      </ul>
+    </header>
+  );
 }
