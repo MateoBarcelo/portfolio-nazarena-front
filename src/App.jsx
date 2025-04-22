38c975ac034cb31deb00930a9b3b80e24d607ee5
MateoBarcelo <mateobarcelo88@gmail.com>
2025-04-22 20:29:50 -0300

    refactor: standardize import statements and update section background for consistency

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,16 @@
-import './App.css'
-import Header from './components/header/Header.jsx'
-import Footer from './components/Footer.jsx'
-import AnimatedRoutes from './components/AnimatedRoutes'
+import "./App.css";
+import Header from "./components/header/Header.jsx";
+import Footer from "./components/Footer.jsx";
+import AnimatedRoutes from "./components/AnimatedRoutes";
 
 function App() {
-
   return (
-    <section className='h-full w-full [background-image:url("/arcana1080.jpg")]'>
+    <section className={`h-full w-full bg-primary-500`}>
       <Header />
       <AnimatedRoutes />
       <Footer />
     </section>
-  )
+  );
 }
 
-export default App
+export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,34 +1,34 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{js,jsx,ts,tsx}",
-  'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}',
-  "./components/**/*.{js,jsx,ts,tsx}"],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}",
+    "./components/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
     extend: {
       colors: {
-        'primary': {
+        primary: {
+          50: "#f8f6f2",
           100: "#D0A9E4",
-          400: '#4D8360',
-          700: '#311A64',
-          900: '#1A1A2E'
+          400: "#4D8360",
+          500: "#5838a1",
+          700: "#311A64",
+          900: "#1A1A2E",
         },
-        'secondary': {
-          300: '#A0E7E5',
-          100: '#D0A9E4',
-          90: '#D0A9E4'
-        }
+        secondary: {
+          300: "#A0E7E5",
+          100: "#D0A9E4",
+          90: "#f0e3fc",
+        },
       },
       fontFamily: {
-        'sans': ['Gilroy', 'sans-serif']
+        sans: ["Gilroy", "sans-serif"],
       },
       backgroundImage: {
-        'profile-pattern': "url('../src/assets/patronnaz2.svg')"
-      }
+        "profile-pattern": "url('../src/assets/patronnaz2.svg')",
+      },
     },
   },
-  plugins: [
-    require('flowbite/plugin'),
-    require("tailwindcss-animate")
-  ]
-}
-
+  plugins: [require("flowbite/plugin"), require("tailwindcss-animate")],
+};
